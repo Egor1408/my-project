2edd406ef82961b12d66cce167d9b431b2510a1c
Egor1408 <egor1408@yandex.ru>
2020-02-06 23:09:21 +0300

    06.02

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 var mainMenu = document.querySelector('.main-menu');
+
 var buttonMenu = document.querySelector('.button-menu');
 buttonMenu.addEventListener('click', function(evt){
     evt.preventDefault();
@@ -10,18 +11,59 @@ closeMenu.addEventListener('click', function(evt) {
     evt.preventDefault();
     mainMenu.classList.remove('main-menu_open');
 });
-
-closeMenu.addEventListener('keydown', function(evt) {
+window.addEventListener('click', function(evt) {   
+    evt.preventDefault(); 
+    if(mainMenu.classList.contains('main-menu_open') && !mainMenu.contains(evt.target)) {   
+        // mainMenu.classList.remove('main-menu_open');
+    }
+});
+document.addEventListener('keydown', function(evt) {    
     if(evt.keyCode === 27) {
-        mainMenu.classList.remove('main-menu_open');
+        mainMenu.classList.remove('main-menu_open');       
     };
 });
 
-
-var mySwiper = new Swiper('.swiper-container', {
-    width:240,
-    pagination: {
-        el: '.swiper-pagination'
-    }
+var openSwiper = document.querySelector('.swiper__button');
+var swiper = document.querySelector('.swiper-wrapper');
+var swiperText = openSwiper.querySelector('span');
+var swiperImg = openSwiper.querySelector('div');
+openSwiper.addEventListener('click', function(evt){
+    evt.preventDefault();
+    if(swiper.classList.contains('open')){
+        swiper.classList.remove('open');
+        swiperText.textContent = "Показать все";
+        swiperImg.style.transform = "rotate(0deg)";    
+    } else {
+        swiper.classList.add('open');
+        swiperText.textContent = "Скрыть";
+        swiperImg.style.transform = "rotate(180deg)";
+    }    
 });
 
+
+
+var breakpoint = window.matchMedia( '(min-width:768px)');
+var mySwiper;
+var breakPointChecked = function() {
+    if (breakpoint.matches === true) {
+        if (mySwiper !== undefined) {
+            mySwiper.destroy(true,true);
+        }
+        return;
+    } else if (breakpoint.matches === false) {
+            return enableSwiper();
+        }
+    };
+
+
+var enableSwiper = function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        width:240,
+        spaceBetween:16,
+        pagination: {
+            el: '.swiper-pagination'
+        }         
+    });
+}
+breakpoint.addListener(breakPointChecked);
+breakPointChecked();
